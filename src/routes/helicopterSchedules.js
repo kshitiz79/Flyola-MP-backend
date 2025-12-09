34b50dd5948ac2a3281241ee80b307a7fa1d4449
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-12-09 11:39:03 +0530

    d

--- src/routes/helicopterSchedules.js
+++ src/routes/helicopterSchedules.js
@@ -3,7 +3,7 @@ const ctrl = require('../controller/helicopterScheduleController');
 
 // Specific routes MUST come before parameterized routes
 router.get('/schedule-by-helipad', ctrl.getScheduleBetweenHelipadDate);
-// router.get('/price-by-day/:id', ctrl.getSchedulePriceByDay);
+router.get('/price-by-da/:id', ctrl.getSchedulePriceByDay);
 
 // General CRUD routes
 router.get('/', ctrl.getHelicopterSchedules);
