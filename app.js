f74d9ff4df12bdd81464c38b4e0540905af537eb
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-12-27 15:27:50 +0530

    d

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const corsOptions = {
     'https://jetserveaviation.com',
     'https://flyola.in',
     'https://www.flyola.in',
+    'https://lightsalmon-pigeon-708896.hostingersite.com',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
